3b4d7c1bbdf51f91b71c47869e6f557650a23f68
Patrick GIRY <patrickgiry@gmail.com>
2019-08-22 17:48:57 +0200

    Add golden master

---
+++ src/test/java/com/gildedrose/GoldenTest.java
@@ -0,0 +1,26 @@
+package com.gildedrose;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class GoldenTest {
+
+    @Test
+    public void check() throws IOException {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        TexttestFixture.main(new String [0]);
+        Assert.assertEquals(getGloden(), out.toString());
+    }
+
+    private String getGloden() throws IOException {
+        return String.join("\n", Files.readAllLines(Paths.get("golden.txt"))) + "\n";
+    }
+
+}
