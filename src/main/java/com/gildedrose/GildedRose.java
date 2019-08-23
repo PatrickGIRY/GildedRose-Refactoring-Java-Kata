4d88c7534509b8a2f88d5d6e4719f036fbadebc2
Patrick GIRY <patrickgiry@gmail.com>
2019-08-23 15:28:49 +0200

    Move functions to `ItemType`

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -16,64 +16,11 @@ class GildedRose {
         }
     }
 
-    private static void handleBackstage(Item item) {
-        increaseQuality(item);
-        descreaseSellIn(item);
-        if (item.sellIn < 11) {
-            increaseQualityIfPossible(item);
-        }
-
-        if (item.sellIn < 6) {
-            increaseQualityIfPossible(item);
-        }
-    }
-
-    private static void handleAgedBrie(Item item) {
-        descreaseSellIn(item);
-        increaseQuality(item);
-    }
-
-    private static void handleStandardItem(Item item) {
-        descreaseQualityIfPossible(item);
-        descreaseSellIn(item);
-        if (item.sellIn < 0) {
-            increaseQualityIfPossible(item);
-        }
-    }
-
-    private static void descreaseSellIn(Item item) {
-        item.sellIn = item.sellIn - 1;
-    }
-
-    private static void increaseQualityIfPossible(Item item) {
-        if (isNotMaxQuality(item)) {
-            increaseQuality(item);
-        }
-    }
-
-    private static void increaseQuality(Item item) {
-        item.quality = item.quality + 1;
-    }
-
-    private static boolean isNotMaxQuality(Item item) {
-        return item.quality < 50;
-    }
-
-    private static void descreaseQualityIfPossible(Item item) {
-        if (isPositiveQuality(item)) {
-            item.quality--;
-        }
-    }
-
-    private static boolean isPositiveQuality(Item item) {
-        return item.quality > 0;
-    }
-
     private enum  ItemType {
         SULFURAS("Sulfuras, Hand of Ragnaros", item -> {}),
-        AGED_BRIE("Aged Brie", GildedRose::handleAgedBrie),
-        BACKSTAGE("Backstage passes to a TAFKAL80ETC concert", GildedRose::handleBackstage),
-        STANDARD("", GildedRose::handleStandardItem);
+        AGED_BRIE("Aged Brie", ItemType::handleAgedBrie),
+        BACKSTAGE("Backstage passes to a TAFKAL80ETC concert", ItemType::handleBackstage),
+        STANDARD("", ItemType::handleStandardItem);
 
         private final String name;
         private final Consumer<Item> updateQuality;
@@ -87,6 +34,59 @@ class GildedRose {
             return Stream.of(values())
                     .filter(itemType -> itemType.name.equals(name))
                     .findFirst().orElse(ItemType.STANDARD);
+        }
+
+        private static void handleBackstage(Item item) {
+            increaseQuality(item);
+            descreaseSellIn(item);
+            if (item.sellIn < 11) {
+                increaseQualityIfPossible(item);
+            }
+
+            if (item.sellIn < 6) {
+                increaseQualityIfPossible(item);
+            }
+        }
+
+        private static void handleAgedBrie(Item item) {
+            descreaseSellIn(item);
+            increaseQuality(item);
+        }
+
+        private static void handleStandardItem(Item item) {
+            descreaseQualityIfPossible(item);
+            descreaseSellIn(item);
+            if (item.sellIn < 0) {
+                increaseQualityIfPossible(item);
+            }
+        }
+
+        private static void descreaseSellIn(Item item) {
+            item.sellIn = item.sellIn - 1;
+        }
+
+        private static void increaseQualityIfPossible(Item item) {
+            if (isNotMaxQuality(item)) {
+                increaseQuality(item);
+            }
+        }
+
+        private static void increaseQuality(Item item) {
+            item.quality = item.quality + 1;
+        }
+
+        private static boolean isNotMaxQuality(Item item) {
+            return item.quality < 50;
+        }
+
+        private static void descreaseQualityIfPossible(Item item) {
+            if (isPositiveQuality(item)) {
+                item.quality--;
+            }
+        }
+
+        private static boolean isPositiveQuality(Item item) {
+            return item.quality > 0;
         }
 
         public void updateQuality(Item item) {
