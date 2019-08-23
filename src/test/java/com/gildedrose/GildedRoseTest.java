17e65dcba9198e62d9e987accb447decffc83c22
Patrick GIRY <patrickgiry@gmail.com>
2019-08-23 15:46:06 +0200

    Test should decrease selin and quality by one day given standard item

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,13 @@ import org.junit.Test;
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    public void should_decrease_selin_and_quality_by_one_day_given_standard_item() {
+        Item[] items = new Item[] { new Item("milk", 4, 15) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("milk", app.items[0].name);
+        assertEquals(3, app.items[0].sellIn);
+        assertEquals(14, app.items[0].quality);
     }
 
 }
