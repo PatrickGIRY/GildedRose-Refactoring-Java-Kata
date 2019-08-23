88b62886ee909a80619d27a0d8aec7db43b0b2d6
Patrick GIRY <patrickgiry@gmail.com>
2019-08-23 16:15:38 +0200

    Test should not change quality and sellin for sulfuras

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -57,4 +57,14 @@ public class GildedRoseTest {
         assertEquals(50, app.items[0].quality);
     }
 
+    @Test
+    public void should_not_change_quality_and_sellin_for_sulfuras() {
+        Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 24, 80)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Sulfuras, Hand of Ragnaros", app.items[0].name);
+        assertEquals(24, app.items[0].sellIn);
+        assertEquals(80, app.items[0].quality);
+    }
+
 }
