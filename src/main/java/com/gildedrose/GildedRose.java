78858fa9aec07233f83d02b00fcaac9ccee08984
Patrick GIRY <patrickgiry@gmail.com>
2019-08-23 10:11:35 +0200

    Move `if(isPositiveQuality(items[i]))`.

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -11,8 +11,8 @@ class GildedRose {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (isPositiveQuality(items[i])) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                    if (isPositiveQuality(items[i])) {
                         items[i].quality = items[i].quality - 1;
                     }
                 }
