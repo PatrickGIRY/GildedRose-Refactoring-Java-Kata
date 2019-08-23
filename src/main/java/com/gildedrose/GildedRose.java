9b5ce7007fe806a356e7c013429dc3d783a7dba1
Patrick GIRY <patrickgiry@gmail.com>
2019-08-23 10:47:47 +0200

    Call `isStandardItem`

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -32,9 +32,7 @@ class GildedRose {
             }
 
             if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")
-                        && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")
-                        && !items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                if (isStandardItem(items[i])) {
                     descreaseQualityIfPossible(items[i]);
                 } else {
                     increaseQualityIfPossible(items[i]);
